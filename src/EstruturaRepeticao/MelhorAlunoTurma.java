8fbed97cb7978ebf90cf4c23492b1d403d269ab9
ana priscilla <anataliateli@gmail.com>
2019-06-02 19:27:45 -0300

    Utilizando While e if  para classificação por nota de aluno.

--- src/EstruturaRepeticao/MelhorAlunoTurma.java
+++ src/EstruturaRepeticao/MelhorAlunoTurma.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Exercício para verificar o melhor aluno e sua nota, dentro de uma classificação.
  */
 package EstruturaRepeticao;
 
